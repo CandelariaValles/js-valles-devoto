02906929affeb2174c3b097965db44d1f615555d
CandelariaValles <116829849+CandelariaValles@users.noreply.github.com>
2023-03-28 18:21:21 -0300

    agrego arrays

--- js/index.js
+++ js/index.js
@@ -48,7 +48,50 @@ function calcularConsumo () {
     
 
 
-
-
 //producto aconsejado
+const productos = [{id: 1,nombre: "Diamond", kw: 24, precio: 2569},
+{id:2, nombre: "Emerald", kw: 28, precio: 2769},
+{id:3, nombre: "Platinum", kw: 34, precio: 2969}
+]
+console.log (productos.length)
+
+const productosSinIva = productos.map((producto) => { return { nombre: producto.nombre, precio: producto.precio / 1.22 } })
+;
+console.log(productosSinIva);
+
+alert ("Los productos disponibles son los siguientes:")
+
+for (const producto of productos) {
+    console.log (producto.kw)
+    alert('La estufa ' + producto.nombre + ' con una capacidad de ' + producto.kw + ' kw. Su precio es de ' + producto.precio + ' euros.');
+}
+const productoConsumoBajo = productos.find((producto)=>producto.kw<25);
+console.log(productoConsumoBajo);
+const productoConsumoMedio = productos.find((producto)=>producto.kw>25&&producto.kw<29);
+console.log(productoConsumoMedio);
+const productoConsumoAlto = productos.find((producto)=>producto.kw>29);
+console.log(productoConsumoAlto);
+
+//producto adapto
+function productoAdapto () {
+    if (consumoEnergetico < 19){
+        const consumoBajo = productos.slice(0,1)[0];
+        alert("El producto que mejor se adapta a ti es la estufa " + consumoBajo.nombre + " de " + consumoBajo.kw +" kw. Su precio es de " + consumoBajo.precio + " euros.");
+        return consumoBajo;
+
+    } else if (consumoEnergetico <24) {
+        const consumoMedio = productos.slice(1,2)[0];
+        alert("El producto que mejor se adapta a ti es la estufa " + consumoMedio.nombre + " de " + consumoMedio.kw +" kw. Su precio es de " + consumoMedio.precio + " euros.");
+        return consumoMedio;
+    
+    } else {
+        const consumoAlto = productos.slice(2,productos.length)[0];
+        alert("El producto que mejor se adapta a ti es la estufa " + consumoAlto.nombre + " de " + consumoAlto.kw +" kw. Su precio es de " + consumoAlto.precio + " euros.");
+        return consumoAlto;
+    };
+}
+
+productoAdapto();
+
+
 
